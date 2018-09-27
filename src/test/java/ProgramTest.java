6eafe42a440cb36cbd81977b2080cb5520a6a1eb
Nasibulin <k.nasibulin@yandex.ru>
2018-09-27 09:35:13 +0300

    -

---
+++ src/test/java/ProgramTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 27.09.18
+ * Time: 10:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProgramTest {
+    @Test
+    public void testSqrt() throws Exception {
+
+    }
+}
