985430a96f0ac417c1be55ee6409f00a628dbdee
Nasibulin <k.nasibulin@yandex.ru>
2018-09-27 16:47:49 +0300

    -

---
+++ src/test/java/DigitsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 27.09.18
+ * Time: 16:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class DigitsTest {
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
