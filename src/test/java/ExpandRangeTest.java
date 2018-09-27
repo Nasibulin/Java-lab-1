d048faff3b6d4098bd6f4aafcc1540cdb996c42e
Nasibulin <k.nasibulin@yandex.ru>
2018-09-27 10:07:41 +0300

    -

---
+++ src/test/java/ExpandRangeTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 27.09.18
+ * Time: 11:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExpandRangeTest {
+    @Test
+    public void testExpandRange() throws Exception {
+
+    }
+}
