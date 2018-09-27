d09e12b5925f6b467dd07e905d2a7fb64b849db4
Nasibulin <k.nasibulin@yandex.ru>
2018-09-27 10:14:10 +0300

    -

--- src/main/java/CollapseRange.java
+++ src/main/java/CollapseRange.java
@@ -61,4 +61,54 @@ public class CollapseRange {
         System.out.println(result.toString());
     }
 
+    public static String CollapseRange(String line){
+        String pattern = "(\\d+)(,*)";
+
+        // Create a Pattern object
+        Pattern r = Pattern.compile(pattern);
+
+        // Now create matcher object.
+        Matcher m = r.matcher(line);
+        Matcher n = r.matcher(line);
+        Matcher o = r.matcher(line);
+        StringBuffer result = new StringBuffer();
+        int check = 0;
+        while (m.find() & n.find() & o.find()) {
+            int i = Integer.parseInt(m.group(1));
+            int j = Integer.parseInt(n.group(1));
+            int k = Integer.parseInt(o.group(1));
+            String s = "";
+            if (n.find(m.end()) & o.find(n.end())) {
+                //    String s = "";
+                //System.out.println(i + "\t" + j + "\t " + k);
+                if ((i == j) & (j == k)) {
+                    s += "" + i;
+                } else if ((i + 1 == j) & (j + 1 == k)) {
+                    s += "";
+                } else if ((i + 1 == j) & (j + 1 != k) & (check + 1 == i)) {
+                    s += "-" + j;
+                } else if ((i + 1 == j) & (j + 1 != k) & (check + 1 != i)) {
+                    s += "," + j;
+                } else if (i + 1 != j) {
+                    s += "," + j;
+                }
+                //System.out.println(i+" "+j+" "+k+" "+check);
+
+                //    m.appendReplacement(result, s);
+            }
+
+            if (o.end() == line.length() & j + 1 == k) s += "-" + k;
+            if (o.end() == line.length() & j + 1 != k) s += "," + k;
+            m.appendReplacement(result, s);
+            check = i;
+
+            //System.out.println(result.toString());
+        }
+        //System.out.println(result.toString());
+        //m.appendTail(result);
+        return result.toString();
+
+
+    }
+
 }
--- src/test/java/CollapseRangeTest.java
+++ src/test/java/CollapseRangeTest.java
@@ -1,5 +1,5 @@
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 /**
  * Created with IntelliJ IDEA.
  * User: Nasibulin
@@ -10,6 +10,7 @@ import org.junit.Test;
 public class CollapseRangeTest {
     @Test
     public void testCollapseRange() throws Exception {
+        assertEquals("1-12,14,17,19-23,25,26,30,35,41",CollapseRange.CollapseRange("1,2,3,4,5,6,7,8,9,10,11,12,14,17,19,20,21,22,23,25,26,30,35,41"));
 
     }
 }
