a5c33c0212f947d5308c2dad9483050d64eff01b
Nasibulin <k.nasibulin@yandex.ru>
2018-09-27 10:13:56 +0300

    -

---
+++ src/test/java/CollapseRangeTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 27.09.18
+ * Time: 11:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class CollapseRangeTest {
+    @Test
+    public void testCollapseRange() throws Exception {
+
+    }
+}
