b0a9b452f5aceab1b2db11938b9711a0cf4499db
Nasibulin <k.nasibulin@yandex.ru>
2018-09-27 10:08:18 +0300

    -

--- src/test/java/ExpandRangeTest.java
+++ src/test/java/ExpandRangeTest.java
@@ -1,5 +1,5 @@
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 /**
  * Created with IntelliJ IDEA.
  * User: Nasibulin
@@ -10,6 +10,6 @@ import org.junit.Test;
 public class ExpandRangeTest {
     @Test
     public void testExpandRange() throws Exception {
-
+    assertEquals("1,2,3,4,5,6,7,8,9,10,11,12,14,17,19,20,21,22,23,25,26,30,35,41",ExpandRange.ExpandRange("1,2,3,4-12,14,17,19-23,25,26,30,35,41"));
     }
 }
