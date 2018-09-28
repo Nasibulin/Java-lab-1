6c5f27694d8b2e3bdf1fe9d9214bbef6104755c7
Nasibulin <k.nasibulin@yandex.ru>
2018-09-28 07:43:57 +0300

    -

---
+++ src/test/java/CircleTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 28.09.18
+ * Time: 8:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class CircleTest {
+    @Test
+    public void testDelta() throws Exception {
+
+    }
+}
