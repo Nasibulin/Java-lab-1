9840befbad2761b3d4469ba74f9a251a07990a7f
Nasibulin <k.nasibulin@yandex.ru>
2018-10-08 21:25:09 +0300

    -

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -1,7 +1,3 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,60 +8,39 @@ import java.io.IOException;
  */
 public class Digits {
     static final int DIG_HEIGHT = 8;
-    static final int DIG_WIDTH = 8;
-    //static final int DIG_QTY = 10;
-    static byte[][] digits = new byte[][]{{32, 32, 42, 42, 42, 32, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 42, 42, 32, 13, 10, 32, 42, 32, 42, 32, 42, 32, 13, 10, 32, 42, 42, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 42, 42, 42, 32, 32, 13, 10}, {32, 32, 32, 42, 32, 32, 32, 13, 10, 32, 32, 42, 42, 32, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10}, {32, 32, 42, 42, 42, 32, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 42, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10, 32, 32, 42, 32, 32, 32, 32, 13, 10, 32, 42, 32, 32, 32, 32, 32, 13, 10, 32, 42, 42, 42, 42, 42, 32, 13, 10}, {32, 32, 42, 42, 42, 32, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 42, 42, 32, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 42, 42, 42, 32, 32, 13, 10}, {32, 32, 32, 32, 42, 32, 32, 13, 10, 32, 32, 32, 42, 42, 32, 32, 13, 10, 32, 32, 32, 42, 42, 32, 32, 13, 10, 32, 32, 42, 32, 42, 32, 32, 13, 10, 32, 32, 42, 32, 42, 32, 32, 13, 10, 32, 42, 42, 42, 42, 42, 32, 13, 10, 32, 32, 32, 32, 42, 32, 32, 13, 10, 32, 32, 32, 42, 42, 42, 32, 13, 10}, {32, 42, 42, 42, 42, 42, 32, 13, 10, 32, 42, 32, 32, 32, 32, 32, 13, 10, 32, 42, 32, 32, 32, 32, 32, 13, 10, 32, 42, 42, 42, 42, 32, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 42, 42, 42, 32, 32, 13, 10}, {32, 32, 32, 42, 42, 32, 32, 13, 10, 32, 32, 42, 32, 32, 32, 32, 13, 10, 32, 42, 32, 32, 32, 32, 32, 13, 10, 32, 42, 42, 42, 42, 32, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 42, 42, 42, 32, 32, 13, 10}, {32, 42, 42, 42, 42, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 42, 32, 32, 13, 10, 32, 32, 32, 32, 42, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10, 32, 32, 32, 42, 32, 32, 32, 13, 10}, {32, 32, 42, 42, 42, 32, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 42, 42, 42, 32, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 42, 42, 42, 32, 32, 13, 10}, {32, 32, 42, 42, 42, 32, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 42, 32, 32, 32, 42, 32, 13, 10, 32, 32, 42, 42, 42, 42, 32, 13, 10, 32, 32, 32, 32, 32, 42, 32, 13, 10, 32, 32, 32, 32, 42, 32, 32, 13, 10, 32, 32, 42, 42, 32, 32, 32, 13, 10}};
+    static final int DIG_WIDTH = 7;
+    static String[] digitArray = new String[8];
 
-    public static void main(String[] args) throws IOException {
-        //System.out.println(Arrays.deepToString(digits));
+    static {
+        digitArray[0] = "  ***     *     ***    ***      *   *****    **   *****   ***    ***  ";
+        digitArray[1] = " *   *   **    *   *  *   *    **   *       *     *   *  *   *  *   * ";
+        digitArray[2] = " *  **    *        *      *    **   *      *          *  *   *  *   * ";
+        digitArray[3] = " * * *    *       *     **    * *   ****   ****       *   ***   *   * ";
+        digitArray[4] = " **  *    *      *        *   * *       *  *   *     *   *   *   **** ";
+        digitArray[5] = " *   *    *     *         *  *****      *  *   *     *   *   *      * ";
+        digitArray[6] = " *   *    *    *      *   *     *   *   *  *   *    *    *   *     *  ";
+        digitArray[7] = "  ***     *    *****   ***     ***   ***    ***     *     ***    **   ";
+    }
+
+
+    public static void main(String[] args) {
+
         System.out.println(main("27092018"));
 
     }
 
-    static void FileToArray() throws IOException {
-        // getBytes from anyWhere
-        // I'm getting byte array from File
-        for (int i = 0; i < 10; i++) {
-            File file = null;
-            FileInputStream fileStream = new FileInputStream(file = new File(System.getProperty("user.dir") + "/src/main/java/" + i));
-
-            // Instantiate array
-            byte[] arr = new byte[(int) file.length()];
-
-            // read All bytes of File stream
-            fileStream.read(arr, 0, arr.length);
-            System.arraycopy(arr, 0, digits[i], 0, arr.length);
-
-//            for (byte X : arr) {
-//                System.out.print(X + ",");
-//            }
-//            System.out.println("");
-        }
-
-    }
-
-    public static String main(String line) throws IOException {
+    public static String main(String line) {
         String result = "";
         char[] charArray = line.toCharArray();
-        //System.out.println(Arrays.deepToString(digits));
-        byte sym;
-        //FileToArray();
+
         for (int row = 0; row < DIG_HEIGHT; row++) {
             for (int column = 0; column < line.length(); column++) {
-                for (int pos = 0; pos < DIG_WIDTH; pos++) {
-                    int k = Character.getNumericValue(charArray[column]);// default cast not work
-                    //System.out.println(charArray[column]);
-                    sym = digits[k][pos + row * (DIG_WIDTH + 1)];
-                    if (sym != 10 & sym != 13) {
-
-                        result += (char) sym;// cast must be explicit
-                    }
-                }
+                int k = Character.getNumericValue(charArray[column]);
+                result += digitArray[row].substring(k * DIG_WIDTH, k * DIG_WIDTH + DIG_WIDTH);
             }
             result += "\n";
         }
         return result;
     }
-
 
 }
